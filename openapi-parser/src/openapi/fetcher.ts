4d8536ea1f1a19b8c90321997bfe0af7229dad48
wldeh <ewthegreat4@gmail.com>
2023-03-29 16:46:37 -0700

    feat: Validate and handle errors for OpenAPI spec fetcher
    The commit adds a new 'isValidUrl' function to check if the given url is valid.
    It also validates the URL before fetching the OpenAPI spec, throwing an error if the URL is invalid. If the response is not ok, another error is thrown. The commit message reflects the addition of valid URL validation and handling of errors during the OpenAPI spec fetch operation.

--- openapi-parser/src/openapi/fetcher.ts
+++ openapi-parser/src/openapi/fetcher.ts
@@ -1,8 +1,14 @@
 import fetch from 'node-fetch'
 
+import { isValidUrl } from './utils'
+
 export async function fetchOpenApiSpec(url: string): Promise<string> {
   try {
+    if (!isValidUrl(url)) throw new Error(`Invalid url: ${url}`)
     const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch OpenAPI spec: ${response.statusText}`)
+    }
     return await response.text()
   } catch (error) {
     throw new Error(`Error fetching OpenAPI spec: ${(error as Error).message}`)
